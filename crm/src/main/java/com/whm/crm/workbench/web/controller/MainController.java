d6bc2e50dc92a3cd95b4db978f3d61e49278437d
whm <1571814958@qq.com>
2022-03-23 15:40:57 +0800

    MainController放错目录 现已修改

--- crm/src/main/java/com/whm/crm/workbench/web/controller/MainController.java
+++ crm/src/main/java/com/whm/crm/workbench/web/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.whm.crm.web.controller;
+package com.whm.crm.workbench.web.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
